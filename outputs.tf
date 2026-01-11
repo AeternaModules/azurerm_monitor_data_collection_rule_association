9f3964fd24fa696fd6334642be34d815de1cce5b
Daniel Allen <dan@aeternagroup.net>
2026-01-11 16:44:02 +0000

    Update to schema version v4.57.0

---
+++ outputs.tf
@@ -0,0 +1,5 @@
+output "monitor_data_collection_rule_associations" {
+  description = "All monitor_data_collection_rule_association resources"
+  value       = azurerm_monitor_data_collection_rule_association.monitor_data_collection_rule_associations
+}
+
