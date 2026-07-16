230a81a838181a1b590248444539dde953436b3a
Daniel Allen <dan@aeternagroup.net>
2026-07-16 14:49:16 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,25 +1,25 @@
 output "monitor_data_collection_rule_associations_id" {
   description = "Map of id values across all monitor_data_collection_rule_associations, keyed the same as var.monitor_data_collection_rule_associations"
-  value       = { for k, v in azurerm_monitor_data_collection_rule_association.monitor_data_collection_rule_associations : k => v.id }
+  value       = { for k, v in azurerm_monitor_data_collection_rule_association.monitor_data_collection_rule_associations : k => v.id if v.id != null && length(v.id) > 0 }
 }
 output "monitor_data_collection_rule_associations_data_collection_endpoint_id" {
   description = "Map of data_collection_endpoint_id values across all monitor_data_collection_rule_associations, keyed the same as var.monitor_data_collection_rule_associations"
-  value       = { for k, v in azurerm_monitor_data_collection_rule_association.monitor_data_collection_rule_associations : k => v.data_collection_endpoint_id }
+  value       = { for k, v in azurerm_monitor_data_collection_rule_association.monitor_data_collection_rule_associations : k => v.data_collection_endpoint_id if v.data_collection_endpoint_id != null && length(v.data_collection_endpoint_id) > 0 }
 }
 output "monitor_data_collection_rule_associations_data_collection_rule_id" {
   description = "Map of data_collection_rule_id values across all monitor_data_collection_rule_associations, keyed the same as var.monitor_data_collection_rule_associations"
-  value       = { for k, v in azurerm_monitor_data_collection_rule_association.monitor_data_collection_rule_associations : k => v.data_collection_rule_id }
+  value       = { for k, v in azurerm_monitor_data_collection_rule_association.monitor_data_collection_rule_associations : k => v.data_collection_rule_id if v.data_collection_rule_id != null && length(v.data_collection_rule_id) > 0 }
 }
 output "monitor_data_collection_rule_associations_description" {
   description = "Map of description values across all monitor_data_collection_rule_associations, keyed the same as var.monitor_data_collection_rule_associations"
-  value       = { for k, v in azurerm_monitor_data_collection_rule_association.monitor_data_collection_rule_associations : k => v.description }
+  value       = { for k, v in azurerm_monitor_data_collection_rule_association.monitor_data_collection_rule_associations : k => v.description if v.description != null && length(v.description) > 0 }
 }
 output "monitor_data_collection_rule_associations_name" {
   description = "Map of name values across all monitor_data_collection_rule_associations, keyed the same as var.monitor_data_collection_rule_associations"
-  value       = { for k, v in azurerm_monitor_data_collection_rule_association.monitor_data_collection_rule_associations : k => v.name }
+  value       = { for k, v in azurerm_monitor_data_collection_rule_association.monitor_data_collection_rule_associations : k => v.name if v.name != null && length(v.name) > 0 }
 }
 output "monitor_data_collection_rule_associations_target_resource_id" {
   description = "Map of target_resource_id values across all monitor_data_collection_rule_associations, keyed the same as var.monitor_data_collection_rule_associations"
-  value       = { for k, v in azurerm_monitor_data_collection_rule_association.monitor_data_collection_rule_associations : k => v.target_resource_id }
+  value       = { for k, v in azurerm_monitor_data_collection_rule_association.monitor_data_collection_rule_associations : k => v.target_resource_id if v.target_resource_id != null && length(v.target_resource_id) > 0 }
 }
 
